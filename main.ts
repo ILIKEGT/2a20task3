26a0b7090f3df00ba55938bcc0c65d9ea31a25e9
ILIKEGT <115674705+ILIKEGT@users.noreply.github.com>
2022-10-20 09:47:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+let value = 0
+input.onButtonPressed(Button.A, function () {
+    value = 9
+    while (value >= 0) {
+        basic.showNumber(value)
+        value += -1
+    }
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    value = 0
+    basic.showNumber(0)
+})
